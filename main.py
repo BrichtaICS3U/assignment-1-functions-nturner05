ddfd4f4cb2d3ee9873f129eff446ae0303151749
NOAH TURNER <S328159827@OCDSB>
2018-03-28 11:04:28 -0400

    not finished changes

--- main.py
+++ main.py
@@ -7,4 +7,21 @@ def CtoF (C):
     """to convert the given number from celsius into fahrenheit"""
     F = (1.8)*C+32
     return F
+def FtoC(F):
+    """to convert the given number from fahrenheit to celcius"""
+    C = (0.55556)*(F-32)
+    return C
 
+x = int(input())
+print('Enter x=1 for Celcius or x=2 for Fahrenheit:')
+if x == 1:
+    print(temperature1)
+elif x == 2:
+    print(temperature2)
+else:
+    print(0)
+
+temperature1 = int(input('Enter your temperature in Farenheit:')
+print(int(round(FtoC(temperature1))))
+temperature2 = int(input('Enter your temperature in Celcius:')
+print(int(round(CtoF(temperature2))))
